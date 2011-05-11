27ba75df411c1b2fb311435fa286fb5832e3677e
Misha Wolfson <mywolfson@gmail.com>
2011-05-11 15:54:25 -0400

    Major code restructuring: use classes

--- js/things.js
+++ js/things.js
@@ -1,11 +1,19 @@
 $(function () {// Package scope
+    // Crockford-style object creation for older JS versions
+    if (typeof Object.create !== 'function') {
+        Object.create = function (o) {
+            function F() {}
+            F.prototype = o;
+            return new F();
+        };
+    }
+    
 
     var things_data$,
+        todos = [],
         filters,
         lists$ = {},
-        NEXT_FOCUS,
-        INBOX_FOCUS,
-        SOMEDAY_FOCUS,
+        foci = {},
         DEBUG = true;
 
     // Debug utilities
@@ -26,83 +34,136 @@ $(function () {// Package scope
         return the_dbg;
     })();
 
+    /* Generic Thing Class: extends jQuery object */
+    function Thing(jQnode) { 
+        var the_thing = Object.create($(jQnode));
 
-    // Return a specific attribute value from a todo object
-    function get_attr(thing_todo$, attr_name) {
-        var attr;
 
-        attr = thing_todo$.find('attribute[name="'+attr_name+'"]');
-        if (attr.attr('type') && attr.attr('type').match(/int/)) {
-            attr = parseInt(attr.text(), 10);
-        } else {
-            attr = attr.text();
-        }
+        // Return a specific attribute value from a todo object
+        the_thing.db_attr = function (attr_name) {
+            var attr;
 
-        return attr;
-    }
-
-    function get_relationship(thing_todo$, rel_name) {
-        var rel,
-            refs = [],
-            idrefs_list,
-            idx;
-
-        rel = thing_todo$.find('relationship[name="'+rel_name+'"]');
-        if (rel.attr('idrefs')) {
-            idrefs_list = rel.attr('idrefs').split(/[ ]+/);
-            for (idx = 0; idx < idrefs_list.length; idx++) {
-                refs.push(parseInt(idrefs_list[idx].replace('z', ''), 10));
+            attr = this.find('attribute[name="'+attr_name+'"]');
+            if (attr.attr('type') && attr.attr('type').match(/int/)) {
+                attr = parseInt(attr.text(), 10);
+            } else {
+                attr = attr.text();
             }
-        }
 
-        return refs;
+            return attr;
+        };
+
+        the_thing.db_relationship = function (rel_name) {
+            var rel,
+                refs = [],
+                idrefs_list,
+                idx;
+
+            rel = this.find('relationship[name="'+rel_name+'"]');
+            if (rel.attr('idrefs')) {
+                idrefs_list = rel.attr('idrefs').split(/[ ]+/);
+                for (idx = 0; idx < idrefs_list.length; idx++) {
+                    refs.push(parseInt(idrefs_list[idx].replace('z', ''), 10));
+                }
+            }
+
+            return refs;
+        };
+
+        return the_thing;
     }
 
-    function make_list(filter_name) {
-        var filter = filters[filter_name],
-            list$ = $('<ul id="' +filter_name +'-list" class="todo-list hidden"></ul>');
+    function ToDo(thing) {
+        var the_todo;
 
-        things_data$.find('object[type="TODO"]').each(function () {
-            var checkbox$;
-            var item$;
+        // Appropriately extend, based on what was given as a starter
+        if ('focus_filter' in thing) { // Already a ToDo
+            return thing;
+        } else if('db_attr' in thing) { // Already a Thing, extend only once
+            the_todo = Object.create(thing);
+        } else { // Build it up from a scratch jQuery object
+            thing = Thing(thing); // First, extend to a Thing
+            the_todo = Object.create(thing); // Then extend to a ToDo
+        }
 
-            if (filter($(this))) {
-                checkbox$ = $('<input type="checkbox" />')
-                    .appendTo('<form></form>');
+        the_todo.make_dom = function () {
+            var checkbox$, item$;
 
-                checkbox$.attr('name', $(this).attr('id'));
-                checkbox$.attr('value', 'completed');
-                if (get_attr($(this), 'status') === 3) {
-                    checkbox$.attr('checked', 'checked');
-                }
+            checkbox$ = $('<input type="checkbox" />');
 
-                item = $('<li></li>')
-                    .append(checkbox$)
-                    .append(get_attr($(this), 'title'))
-                    .appendTo(list$);
-                if (filters.today($(this))) {
-                    item.addClass('today');
-                }
+            checkbox$.attr('name', this.attr('id'));
+            checkbox$.attr('value', 'completed');
+            if (this.db_attr('status') === 3) {
+                checkbox$.attr('checked', 'checked');
+            }
+
+            item$ = $('<li></li>')
+                .append(checkbox$)
+                .append(this.db_attr('title'));
+
+            if (foci.today.filter(this)) {
+                item$.addClass('today');
+            }
+
+            return item$;
+        };
+
+        return the_todo;
+    }
+
+    // Focus types
+
+    function Focus() {
+        var the_focus = {};
+        
+        the_focus.filter = function (todo) {
+            return true;
+        };
+
+        return the_focus;
+    }
+
+    function CategoryFocus(key) {
+        var the_category_focus = Object.create(Focus()),
+            _constant;
+        
+        _constant = parseInt(things_data$.find('object[type="FOCUS"] ' +
+                                  'attribute[name="identifier"]:contains(' +
+                                  key + ')')
+                            .parent()
+                            .attr('id')
+                            .replace('z', ''), 10);
+
+        the_category_focus.filter = function (todo) {
+            return todo.db_relationship('focus').indexOf(_constant) >=0;
+        };
+
+        return the_category_focus;
+    }
+
+    function TodayFocus() {
+        var the_today_focus = Object.create(Focus());
+
+        the_today_focus.filter = function (todo) {
+            return todo.db_attr('focustype') === 65536;
+        };
+
+        return the_today_focus;
+    }
+
+    function make_list(focus_name) {
+        var focus = foci[focus_name],
+            list$ = $('<ul id="' + focus_name + '-list" class="todo-list"></ul>')
+                     .hide();
+
+        $.each(todos, function () {
+            if (focus.filter(this)) {
+                list$.append(this.make_dom());
             }
         });
 
         return list$;
     }
-
-    filters = {
-        inbox: function (thing_todo$) {
-            return get_relationship(thing_todo$, 'focus').indexOf(INBOX_FOCUS) >=0;
-        },
-        today: function (thing_todo$) {
-            return get_attr(thing_todo$, 'focustype') === 65536;
-        },
-        next: function (thing_todo$) {
-            return get_relationship(thing_todo$, 'focus').indexOf(NEXT_FOCUS) >=0;
-        },
-        someday: function (thing_todo$) {
-            return get_relationship(thing_todo$, 'focus').indexOf(SOMEDAY_FOCUS) >=0;
-        }
-    };
 
     function hide_lists() {
         for (var lx in lists$) {
@@ -112,20 +173,18 @@ $(function () {// Package scope
         }
     }
 
-    function load_constants() {
-        function load_constant(key) {
-            return parseInt(
-                things_data$.find('object[type="FOCUS"] ' +
-                                  'attribute[name="identifier"]:contains(' +
-                                  key + ')')
-                            .parent()
-                            .attr('id')
-                            .replace('z', ''), 10);
-        }
+    function load_db() {
 
-        NEXT_FOCUS = load_constant('FocusNextActions');
-        INBOX_FOCUS = load_constant('FocusInbox');
-        SOMEDAY_FOCUS = load_constant('FocusMaybe');
+        // Create the focus objects
+        foci.next    = CategoryFocus('FocusNextActions');
+        foci.inbox   = CategoryFocus('FocusInbox');
+        foci.someday = CategoryFocus('FocusMaybe');
+        foci.today   = TodayFocus();
+
+        // Load the todos 
+        things_data$.find('object[type="TODO"]').each(function () {
+            todos.push(ToDo(this));
+        });
     }
 
     function make_lists() {
@@ -159,7 +218,7 @@ $(function () {// Package scope
     function create_content(data) {
         dbg.log('creating content');
         things_data$ = $(data);
-        load_constants();
+        load_db();
         make_lists();
     }
 
@@ -171,10 +230,7 @@ $(function () {// Package scope
 
         // Pull the database file
         $.ajax('../Database.xml', {
-            success: function (data) {
-                dbg.log('success');
-                create_content(data);
-            },
+            success: create_content,
             error: function (jqXHR, textStatus, errorThrown) {
                 dbg.error(errorThrown);
             },
